875fd931c373926ed1c503b80ad06c217a719368
juanpegarcia11 <juanpegarciam11@gmail.com>
2023-08-12 13:00:49 +0200

    Data Ingestion Stage

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.WineQualityMLFlow import logger
+from WineQualityMLFlow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from WineQualityMLFlow.config.configuration import ConfigurationManager
 
 logger.info("Initializing custom logging")
+
+STAGE_NAME = "Data Ingestion Stage"
+logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<\n")
+obj = DataIngestionTrainingPipeline()
+logger.info(f">>>>>> Stage {STAGE_NAME} executing... <<<<<<\n")
+obj.main()
+logger.info(f">>>>>> Stage {STAGE_NAME} completed! <<<<<<\n=========================\n")
+
